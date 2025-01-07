f5c1f6f8e81fa93647f30e68982e378c89afc89d
shuxratov2024 <shukhratovamir010@gmail.com>
2025-01-07 20:02:54 +0500

    java

--- Main.java
+++ Main.java
@@ -4,48 +4,48 @@ public class Main {
 
         //N1
 
-//        String name ="Amir";
-//        System.out.println(name);
+        String name ="Amir";
+        System.out.println(name);
 //N2
 
-//        String FirstName = "Amir ";
-//        String LastName = "Shuxratov ";
-//        String DadName = "Furqatovich";
-//        System.out.println(LastName+FirstName+DadName);
+        String FirstName = "Amir ";
+        String LastName = "Shuxratov ";
+        String DadName = "Furqatovich";
+        System.out.println(LastName+FirstName+DadName);
 
-        //N3
-//            for(int i = 46; i<=56; i++){
-//                System.out.println(i);
-//            }
-        //N4
+    //    N3
+            for(int i = 46; i<=56; i++){
+                System.out.println(i);
+            }
+  //      N4
 
-//        for(int i = 34; i<=78; i++) {
-//            System.out.println(i+""+""+"");
-//        }
+        for(int i = 34; i<=78; i++) {
+            System.out.println(i+""+""+"");
+        }
 
 //Format
 
-//        String name="Amir";
-//        String country = "uzbekistan";
-//       String lastName = "Shuxartov";
-//        int age= 15;
-//        String company = "Solo";
-//        char percentSign = '%';
-//
-//        String formatString = String.format("my name is %s. I am from %s. I am %s. years old. I am work %s;",
-//        name,country,age,company,percentSign);
-//
-//        System.out.println(formatString);
+        String firstNAme="Amir";
+        String country = "uzbekistan";
+       String lastName = "Shuxartov";
+        int age= 15;
+        String company = "Solo";
+        char percentSign = '%';
+
+        String formatString = String.format("my name is %s. I am from %s. I am %s. years old. I am work %s;",
+        firstNAme,country,age,company,percentSign);
+
+        System.out.println(formatString);
 
 
 //Uppercase and Lowercase
-//        String name = "Amir Shuxratov";
-//        System.out.println(name.toUpperCase());
-//        System.out.println(name.toLowerCase());
+        String Amir = "Amir Shuxratov";
+        System.out.println(Amir.toUpperCase());
+        System.out.println(Amir.toLowerCase());
 
-        //Tengliklar
-//        String string1 = "amir";
-//        String string2 = "amir";
-//        System.out.println(string1 == string2);
+      //  Tengliklar
+        String string1 = "amir";
+        String string2 = "amir";
+        System.out.println(string1 == string2);
     }
 }
